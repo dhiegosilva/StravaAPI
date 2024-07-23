ed62aaff2623f842b5aae53705231b8a206b057b
dhiego <dhiego.silva@hotmail.com>
2024-07-23 23:45:28 +0200

    swagger added, openapi

--- src/main/java/com/strava/stravaapi/UserController.java
+++ src/main/java/com/strava/stravaapi/UserController.java
@@ -1,10 +1,16 @@
 package com.strava.stravaapi;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 //@RequestMapping("/users")
 public class UserController {
+
 
     @PostMapping("/users/import")
     public String importUser(@RequestBody User user) {
@@ -12,13 +18,32 @@ public class UserController {
         return "User imported: " + user.getName();
     }
 
-//    @GetMapping("/hello/{name}")
-//    public String sayHello(@PathVariable String name) {
-//        return String.format("Hello, %s!", name);
-//    }
-
+    @Operation(summary = "Get a greeting message")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Greeting.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid request"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("Hello, %s!", name);
+    }
+
+    class Greeting {
+        private String message;
+
+        public Greeting(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
     }
 }
